0dc18433b6e111c0d358e8cec58a037840e743d2
Curious Cuttlefisch <curious.cuttlefisch@frostfeather.net>
2018-07-09 13:08:09 -0700

    added a few files. Still getting the hang of commits and such.

---
+++ src/main/java/com/curiouscuttlefisch/moarvanilla/Main.java
@@ -0,0 +1,12 @@
+package com.curiouscuttlefisch.moarvanilla;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
+import com.curiouscuttlefisch.moarvanilla.util.Reference;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main 
+{
+	@Instance
+	public static Main instance;
+}
---
+++ src/main/java/com/curiouscuttlefisch/moarvanilla/util/Reference.java
@@ -0,0 +1,12 @@
+package com.curiouscuttlefisch.moarvanilla.util;
+
+public class Reference 
+{
+    public static final String MOD_ID = "moarvanilla";
+    public static final String NAME = "Moar Vanilla";
+    public static final String VERSION = "0.1";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "";
+    public static final String COMMON_PROXY_CLASS = "";
+    
+}
